948ebe25980e6082b4211be173a1cd9cd0de9fd3
Suravit Chatapat <suravit@madappe.com>
2017-03-06 19:00:08 +0700

    POp

--- html/view/home.php
+++ html/view/home.php
@@ -1,5 +1,18 @@
 <div class="home">
-	<div class="banner">
-			<h1>Welcome to Abac Corner</h1>
-	</div>
+    <div class="banner">
+        <h1>Welcome to Abac Corner</h1>
+    </div>
+    <div class="menu2">
+        <div class="container">
+            <div class="roll">
+                <div class="col-md-6 col-xs-12 ">
+                    <h2 class="text-center"">About Abac Corner</h2>
+                    <p>AbacCorner aims to be the best community for all ABAC students. We would be a center of all information in ABAC.
+                       At first time, you' ll be a part of ABAC you might not know what should be started as beginning international university students. AbacCorner would be a trainer to guide you until you're done from here.</p>
+                </div>
+                <div class="col-md-6 col-xs-12 "><div class="bg-img"></div></div>
+
+            </div>
+        </div>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -41,12 +41,87 @@
 
     <body ng-app="routerApp">
       <!-- header -->
-      <div ng-include src="'/html/view/header.php'"></div>
-          <div ui-view></div>
+        <header>
+            <div class="navbH">
+                <div class="navDesktop hidden-xs hidden-sm">
+                    <nav class="navbar navbar-inverse">
+                        <div class="container-fluid">
+                            <div class="navbar-header">
+                                <a class="navbar-brand" ui-sref="home"><img src="resources/images/logo.png" alt=""></a>
+                                <div class="ribbon"></div>
+                            </div>
+                            <ul class="nav navbar-nav">
+                                <li ui-sref-active="active"><a ui-sref="home" onclick="setStyle()">Home</a></li>
+                                <li ui-sref-active="active"><a ui-sref="acadamic" onclick="setStyle()">Acadamics</a></li>
+                                <li ui-sref-active="active"><a ui-sref="tips" onclick="setStyle()">Tips</a></li>
+                                <li ui-sref-active="active"><a ui-sref="links" onclick="setStyle()">Links</a></li>
+                                <li ui-sref-active="active"><a ui-sref="contactus" onclick="setStyle()">Contact Us</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a ui-sref="signup"> Sign Up</a></li>
+                                <li class="signB"></li>
+                                <li><a ui-sref="login"> Login</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
+                <div class="navMobile visible-xs visible-sm" >
+                    <nav class="navbar navbar-inverse">
+                        <div class="text-left">
+                            <a class="navbar-brand" ui-sref="#">Abac Corner</a>
+                        </div>
+                        <div class="text-right">
+                            <a href="#menu">
+                                <i class="fa fa-bars"></i>
+                            </a>
+                        </div>
+                    </nav>
+
+                    <header>
+                        <nav id="menu">
+                            <ul>
+                                <li><a ui-sref="home" onclick="setStyle()">Home</a></li>
+                                <li><a ui-sref="acadamic" onclick="setStyle()">Academics</a></li>
+                                <li><a ui-sref="tips" onclick="setStyle()">Contact</a></li>
+                                <li><a ui-sref="links" onclick="setStyle()">Contact</a></li>
+                                <li><a ui-sref="contactus" onclick="setStyle()">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </header>
+                </div>
+            </div>
+        </header>
+      <!--end header-->
+
+        <div ui-view id="heightBody"></div>
         <!-- footer -->
-        <div ng-include src="'/html/view/footer.php'"></div>
+      <!-- Start Footer -->
+        <footer>
+            <div class="container text-center social">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <a href="mailto:abaccorner@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i> <span style="color:white;" class="ft1">abaccorner
+           @gmail.com</span></a>
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <a href="https://www.facebook.com/abaccorner"> <i class="fa fa-facebook fb" aria-hidden="true"></i>
+                            </button>
+                            <span style="color:white;" class="ft1">AbacCorner</span></a>
+
+                    </div>
+                    <div class="col-md-4 col-sm-12 col-xs-12">
+                        <a href="contactus.html"> <i class="fa fa-commenting" aria-hidden="true"></i>
+                            <span style="color:white;" class="ft1">Contact US</span></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+      <!-- End Footer -->
+
 
     </body>
+
     <script type="text/javascript">
         jQuery(document).ready(function( $ ) {
             $("#menu").mmenu({
@@ -69,6 +144,22 @@
                     "title": "Abac Corner"
                 }
             });
+
+            setStyle();
         });
+
+        function setStyle() {
+            setTimeout(function() {
+                var height = $(document).height();
+                var heightBody = $('#heightBody').height();
+                var height80 = height * 80 / 100;
+
+                if (heightBody < height80) {
+                    $("footer").css("position", "fixed");
+                }else {
+                    $("footer").css("position", "relative");
+                }
+            }, 100);
+        }
     </script>
 </html>
